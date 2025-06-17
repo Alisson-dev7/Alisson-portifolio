6a7777d5a05332373a5f7bdceebbfc2354f799d7
Alisson-dev7 <josealisson977.dev@gmail.com>
2025-06-17 19:34:14 -0300

    links

--- js/script.js
+++ js/script.js
@@ -9,3 +9,12 @@ function menuShow(){
 function vangogh (){
     window.location.href= "https://vangogh7.netlify.app";
 }
+function insta(){
+    window.location.href="https://www.instagram.com/alisson_dev7/?utm_source=ig_web_button_share_sheet";
+}
+function whats(){
+    window.location.href="https://wa.link/h2knx4"
+}
+function git (){
+    window.location.href="https://github.com/Alisson-dev7"
+}
